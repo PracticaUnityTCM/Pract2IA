485e519eec4ad84e4ebe06487b0f3c2fed72b6e6
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2018-03-06 17:04:12 +0100

    Added the roomba the mouse and the dust mouse falta flee when see roomba

---
+++ RoombaWorld_STU/Assets/FSMs/FSM_WanderRoomba.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+using Steerings;
+[RequireComponent(typeof(FSM_RouteExecutor))]
+public class FSM_WanderRoomba : FiniteStateMachine
+{
+
+    public enum States { INITIAL, MISSINGPOINT, GOTOPOINT };
+    public States currentState = States.INITIAL;
+    private FSM_RouteExecutor RouterExecutor;
+    private ROOMBA_Blackboard RoombaBB;
+    public Vector3 target;
+    public GameObject GOTarjet;
+    // Use this for initialization
+    void Start()
+    {
+        RouterExecutor = GetComponent<FSM_RouteExecutor>();
+        RoombaBB = GetComponent<ROOMBA_Blackboard>();
+        RouterExecutor.enabled = false;
+        GOTarjet = new GameObject("TargetRoomba");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                ChangeState(States.MISSINGPOINT);
+                
+                break;
+            case States.MISSINGPOINT:
+                target = RandomLocationGenerator.RandomPatrolLocation();       
+                
+                GOTarjet.transform.position = target;
+                ChangeState(States.GOTOPOINT);
+                break;
+            case States.GOTOPOINT:
+                if (RouterExecutor.currentState == FSM_RouteExecutor.State.TERMINATED)
+                    ChangeState(States.MISSINGPOINT);
+                break;
+        }
+    }
+    void ChangeState(States newState)
+    {
+        switch (currentState)
+        {
+        
+            case States.GOTOPOINT:
+                RouterExecutor.Exit();
+                RouterExecutor.target = null;
+                RouterExecutor.enabled = false;
+                break;
+        }
+        switch (newState)
+        {
+           
+            case States.GOTOPOINT:
+                RouterExecutor.ReEnter();
+                RouterExecutor.target = GOTarjet;
+                RouterExecutor.enabled = true;
+                break;
+        }
+        currentState = newState;
+    }
+}
---
+++ RoombaWorld_STU/Assets/SpawnMouses.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMouses : MonoBehaviour {
+    public GameObject[] spawnPoints;
+    public float timer=0;
+    public float interval=20f;
+    public GameObject mousePrefab;
+	// Use this for initialization
+	void Start () {
+        spawnPoints = GameObject.FindGameObjectsWithTag("EXTERNAL_SPAWNER");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > interval)
+        {
+            Instantiate(mousePrefab, getRandomSpawnerLocation().transform.position, Quaternion.identity);
+            timer = 0;
+        }
+	}
+    private GameObject getRandomSpawnerLocation()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)];
+    }
+}
