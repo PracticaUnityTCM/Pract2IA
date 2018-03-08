f7b0ad58480a733d9ee2bdc3dbc751b26377b719
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2018-03-08 19:19:06 +0100

    aDDED GIZMOS TO SCENE OBJ

--- RoombaWorld_STU/Assets/FSMs/FSM_MouseFlee.cs
+++ RoombaWorld_STU/Assets/FSMs/FSM_MouseFlee.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FSM;
+namespace FSM
+{
+    [RequireComponent(typeof(FSM_RouteExecutor))]
+    [RequireComponent(typeof(FSM_Mouse))]
+    public class FSM_MouseFlee : MonoBehaviour
+    {
+        public enum State { IINITAL, NORAMAL, FLEE }
+        public State currentState;
 
-public class FSM_MouseFlee : MonoBehaviour {
+        // Use this for initialization
+        void Start()
+        {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
